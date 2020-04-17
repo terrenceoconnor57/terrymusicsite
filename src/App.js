15f6f3bb7ea3b349b094e3c2da27d4c5fce480bb
terrenceoconnor57 <60070754+terrenceoconnor57@users.noreply.github.com>
2020-04-17 17:04:57 -0400

    Created array for music player

--- src/App.js
+++ src/App.js
@@ -1,8 +1,75 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render() {
+    const playlist = [
+  {
+    url: 'lead groove with piano.mp3',
+    cover: 'mee.png',
+    title: 'Someday',
+    artist: [
+      'Terry',
+      'Dennis'
+    ]
+  },
+  {
+    url: 'Exit Wound.mp3',
+    cover: 'mee.png',
+    title: 'Exit Wound',
+    artist: [
+      'Terry',
+      'Dennis'
+    ]
+  },
+  {
+    url: 'new kings.mp3',
+    cover: 'mee.png',
+    title: 'Kings',
+    artist: [
+      'Terry',
+      'Dennis'
+    ]
+  },
+  {
+    url: 'shine_.mp3',
+    cover: 'mee.png',
+    title: 'Imperfect Shine',
+    artist: [
+      'Terry',
+      'Dennis'
+    ]
+  },
+  {
+    url: 'killing me final.mp3',
+    cover: 'mee.png',
+    title: 'Killing Me',
+    artist: [
+      'Terry',
+      'Dennis'
+    ]
+  },
+  {
+    url: 'Thousand Miles Gone.mp3',
+    cover: 'mee.png',
+    title: 'Thousand Miles Gone',
+    artist: [
+      'Terry',
+      'Dennis'
+    ]
+  },
+  {
+    url: 'Dont Wake Me.mp3',
+    cover: 'mee.png',
+    title: 'Don\'t Wake Me',
+    artist: [
+      'Terry',
+      'Dennis'
+    ]
+  },
+
+]
   return (
     <div className="App">
     <div className = "App-header">
@@ -13,12 +80,13 @@ function App() {
     <li className = 'cent'><a className = 'anc' href ="">Songs</a></li>
     <li className = 'cent'><a className = 'anc' href ="">Lyrics</a></li>
     </ul>
-    <div className = 'music'>
+</div>
+
+  <h1 className = 'mainline'>Our Music</h1>
 
     </div>
-</div>
-    </div>
   );
+}
 }
 
 export default App;
